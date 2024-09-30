be91836e41c82fd61940d1e31b41c0413ae24532
thienhieu-hoang <thien-hieu.hoang@outlook.com>
2024-09-30 12:00:35 -0400

    test loading and saving models for transfer

--- Torch_code/helper/utils_transfer.py
+++ Torch_code/helper/utils_transfer.py
@@ -31,8 +31,8 @@ class FineTuneModel(nn.Module):
         self.conv5 = nn.Conv2d(32, 16, kernel_size=(5, 5), stride=(1, 1), padding=(2, 2))
         
         # Add 3 more layers
-        self.conv6 = nn.Conv2d(16, 8, kernel_size=(3, 3), stride=(1, 1), padding=(2, 2))
-        self.conv7 = nn.Conv2d( 8, 1, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        self.conv6 = nn.Conv2d(16, 8, kernel_size=(5, 5), stride=(1, 1), padding=(2, 2))
+        self.conv7 = nn.Conv2d( 8, 1, kernel_size=(5, 5), stride=(1, 1), padding=(5, 5))
         
     def forward(self, x):
         x = self.normalization(x)
